873c3767b94f9afb8fb970d02737a1f6258abd2c
Kisung Son <eson@samsung.com>
2018-05-19 21:47:33 +0000

    Update 20180520fuction3.cpp

--- 20180520fuction3.cpp
+++ 20180520fuction3.cpp
@@ -2,58 +2,23 @@
 
 int num[10];
 
-int sort(int start, int end)
+int bobble_sort(int delimiter, int start, int end)
 {
-	int temp, cnt = 0;
-	if (start < end) {
-		temp = start;
-		start = end;
-		end = temp;
-	}
 
-	for (int k = end; k > start; k--) {
-		for (int i = start; i < k; i++) {
-			if (num[i] > num[k + 1]) {
-				temp = num[i];
-				num[i] = num[i + 1];
-				num[i + 1] = temp;
-				cnt++;
-			}
-		}
-	}
-	return cnt;
+
+	
+	
+
 }
 
-int chk_sorted(int check, int start, int end)
+int chk_sorted(int delimiter, int start, int end)
 {
-	int temp = 1;
-	if (start > end) {
-		temp = start;
-		start = end;
-		end = temp;
-	}
-	switch (check) {
-	case 1:			
-		for (int i = start; i < end; i++) {
-			if (num[i] > num[i + 1]) {
-				temp = 0;
-				break;
-			}
-		}
-		break;
-	case 2:				
-		for (int i = start; i < end; i++) {
-			if (num[i] < num[i + 1]) {
-				temp = 0;
-				break;
-			}
-		}
-		break;
-	default:			
-		temp = 0;
-		break;
-	}
-	return temp;
+	
+	
+
+
+
+
 }
 
 int main(void)
@@ -66,7 +31,7 @@ int main(void)
 	temp = chk_sorted(1, 3, 6);
 
 	if (temp == 0) {
-		temp1 = sort(0, 9);
+		temp1 = bobble_sort(0, 9);
 	}
 	printf("%d\n", temp1);
 	return 0;
