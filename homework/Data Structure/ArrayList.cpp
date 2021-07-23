fd592e7735b8b5ee64376af71d4cc2226e3fc34f
Hyunsoo Son <39242207+ehsson@users.noreply.github.com>
2021-07-23 13:53:47 +0900

    Update ArrayList.cpp

--- homework/Data Structure/ArrayList.cpp
+++ homework/Data Structure/ArrayList.cpp
@@ -57,3 +57,62 @@ int LCount(List* plist)
 {
 	return plist->numOfData;
 }
+
+// using class
+
+#include <iostream>
+#include "ArrayList.h"
+using namespace std;
+
+ArrayList::ArrayList()
+	:numOfData(0), curPosition(-1)
+{}
+
+void ArrayList::LInsert(LData data)
+{
+	if (numOfData >= LIST_LEN) {
+		cout << "저장이 불가합니다.\n";
+		return;
+	}
+
+	arr[numOfData] = data;
+	numOfData++;
+}
+
+bool ArrayList::LFirst(LData &pdata)
+{
+	if (numOfData == 0)
+		return false;
+
+	curPosition = 0;
+	pdata = arr[curPosition];
+	return true;
+}
+
+bool ArrayList::LNext(LData &pdata)
+{
+	if (curPosition >= numOfData - 1)
+		return false;
+
+	curPosition++;
+	pdata = arr[curPosition];
+	return true;
+}
+
+void ArrayList::LRemove()
+{
+	int rpos = curPosition;
+	int num = numOfData;
+	LData rdata = arr[rpos];
+
+	for (int i = rpos; i < num - 1; i++)
+		arr[i] = arr[i + 1];
+
+	numOfData--;
+	curPosition--;
+}
+
+int ArrayList::LCount()
+{
+	return numOfData;
+}
